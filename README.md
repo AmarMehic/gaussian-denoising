6884b93e7b5094d4a85185e9724c0f666d6c5f48
Amar Mehic <ammar.mehic@gmail.com>
2026-05-30 12:00:32 +0200

    Removing previous renderer try and switching to new one.

--- README.md
+++
@@ -1,5 +0,0 @@
-## Setup
-Clone the renderer: (do not forget to cite)
-```bash
-git clone "https://github.com/ubc-vision/stochasticsplats.git"
-```
--- scripts/renderer_overlay/src/core/xrbuddy.cpp
+++
@@ -1,7 +0,0 @@
-/*
-    Headless overlay: empty translation unit. The full XrBuddy implementation
-    is inlined in the stub header. This .cpp exists only so the CMakeLists.txt
-    sources list (which references xrbuddy.cpp) does not need to change.
-*/
-
-#include "xrbuddy.h"
